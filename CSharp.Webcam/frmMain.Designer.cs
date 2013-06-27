5b3d22d47ab2f12623d71a3c82cd40cebb3f2e7e
Adam K Dean <adamkdean@googlemail.com>
2013-06-27 12:14:30 +0100

    Added UI

--- CSharp.Webcam/frmMain.Designer.cs
+++ CSharp.Webcam/frmMain.Designer.cs
@@ -28,20 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.Location = new System.Drawing.Point(12, 12);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(320, 240);
+            this.pbPreview.TabIndex = 1;
+            this.pbPreview.TabStop = false;
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.Color.White;
+            this.txtLog.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.Location = new System.Drawing.Point(338, 12);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(380, 241);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.TabStop = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 470);
+            this.ClientSize = new System.Drawing.Size(730, 264);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.pbPreview);
             this.Name = "frmMain";
-            this.Text = "CSharp.Webcam";            
+            this.Text = "CSharp.Webcam";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- CSharp.Webcam/frmMain.cs
+++ CSharp.Webcam/frmMain.cs
@@ -6,15 +6,59 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
+using Touchless.Vision.Camera;
 
 namespace CSharp.Webcam
 {
     public partial class frmMain : Form
     {
+        private List<Camera> availableCameras;
+        private Camera selectedCamera;
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            WriteLog("Initializing...");
+
+            var timer = new System.Timers.Timer { Interval = (1000f) };
+            timer.Elapsed += new ElapsedEventHandler((o, a) => Start());
+            timer.Elapsed += new ElapsedEventHandler((o, a) => timer.Enabled = false);
+            timer.Enabled = true;
+        }
+
+        private void Start()
+        {
+            availableCameras = new List<Camera>(CameraService.AvailableCameras);
+            availableCameras.ForEach(x => WriteLog("Camera found: {0}", x.Name));
+
+            selectedCamera = availableCameras[0];
+            WriteLog("Selected camera: {0}", selectedCamera.Name);
+        }
+
+        private void WriteLog(string format, params object[] args)
+        {
+            WriteLog(string.Format(format, args));
+        }
+
+        private void WriteLog(string s)
+        {
+            if (InvokeRequired)
+            {
+                Action<string> action = new Action<string>(WriteLog);
+                Invoke(action, s);
+                return;
+            }
+
+            string prefix = (txtLog.Text == "") ? "" : "\r\n";
+            txtLog.Text += string.Format("{0}{1}", prefix, s);
+            txtLog.SelectionStart = txtLog.Text.Length;
+            txtLog.ScrollToCaret();
         }
     }
 }
